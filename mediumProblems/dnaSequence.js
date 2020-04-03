75279a27433467256777f9993fb05a0705a17c6e
Adam Ross <adamgarrettross@gmail.com>
2020-04-03 14:15:15 -0500

    DNA youtube video done.

--- mediumProblems/dnaSequence.js
+++ mediumProblems/dnaSequence.js
@@ -1,42 +1,31 @@
-// Map the dna sequence
-
+// Using an object
 // const dna = (str) => {
 //     const res = []
-//     const scene = {}
-//     let count = 0
-//     for(let i = 0; i + 9 < str.length; i++) {
-//         let sequence = str.slice(i, i+10)
-//         scene[sequence] = scene[sequence] + 1 || 1
+//     const seen = {}
 
-//         if(scene[sequence] === 2) {
-//             res.push(sequence)
+//     for(let i = 0; i + 9 < str.length; i++) {
+//         let seq = str.slice(i, i+10)
+//         seen[seq] = seen[seq] + 1 || 1
+
+//         if(seen[seq] === 2) {
+//             res.push(seq)
 //         }
 //     }
-
-//     console.log(str.length)
 //     return res
 // }
 
+// using sets
+
 const dna = (str) => {
-    const res = []
-    const compare = []
-    const final = []
+    let pastSeq = new Set()
+    let res = new Set()
 
-    for(let i = 0; i + 9 < str.length; i++) {
-        let sequence = str.slice(i, i+10)
-        if(!compare.includes(sequence)) {
-            compare.push(sequence)
-        } else {
-            res.push(sequence)
-        }
+    for(let i = 0; i <= str.length - 10; i++) {
+        let comparable = str.substring(i, i+10)
+        pastSeq.has(comparable) ? res.add(comparable) : pastSeq.add(comparable)
     }
-
-    const set = new Set(res)
-    set.forEach(num => {
-        final.push(num)
-    })
-    return final
+    return [...res]
 }
 
-const dnaSequence = 'AAAAACCCCCAAAAACCCCCCAAAAAGGGTTTAAAAACCCCCAAAAACCCCCCAAAAAGGGTTT'
-console.log(dna(dnaSequence))
+const str = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
+console.log(dna(str))
